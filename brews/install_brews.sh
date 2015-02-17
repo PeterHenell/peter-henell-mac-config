bd1949470eda0664060467b22a0893287f6e8ccb
Peter Henell <peter.henell@klarna.com>
2015-02-17 16:13:06 +0100

    adding brews

--- brews/install_brews.sh
+++ brews/install_brews.sh
@@ -1,5 +1,7 @@
 brew update
 brew doctor
 
-brew install stow
-brew install python3
+brew install stow python3 htop-osx
+brew install ansible git libyaml openssl pkg-config rlwrap subversion
+brew install brew-cask gnu-sed maven pcre readline sqlite wget
+brew install gdbm htop-osx nginx pgtune python rename stow xz postgresql Caskroom/cask/pgadmin3 Caskroom/cask/keepassx
