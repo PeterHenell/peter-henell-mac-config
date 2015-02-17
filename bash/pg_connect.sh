db4b07e1b7ff5961688649a109c57d64c4a4c7f9
Peter Henell <peter.henell@klarna.com>
2015-02-17 16:24:50 +0100

    small fixes

--- bash/pg_connect.sh
+++ bash/pg_connect.sh
@@ -3,11 +3,11 @@ source ~/.bash_functions
 TEMPLATE='postgresql://$username:$password@$host:5432/$db_name?sslmode=prefer'
 
 # username host db_name
-s="peter.henell localhost devdb"
+s="peter.henell localhost bix_dev"
 
-pg_connections[0]="postgres localhost dev_dbv"
-pg_connections[1]='peter remote_host live_db"
-pg_connections[2]=""
+pg_connections[0]="postgres localhost db_dev"
+pg_connections[1]="peter.henell test-server db_live"
+pg_connections[2]="peter.henell localhost db_dev"
 
 
 key=(${s// / })
