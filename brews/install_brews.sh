37c4e2dff0ed8397497f63254ae6096271d93f11
Peter Henell <peter.henell@klarna.com>
2015-02-18 14:34:52 +0100

    adding tree

--- brews/install_brews.sh
+++ brews/install_brews.sh
@@ -3,5 +3,5 @@ brew doctor
 
 brew install stow python3 htop-osx
 brew install ansible git libyaml openssl pkg-config rlwrap subversion
-brew install brew-cask gnu-sed maven pcre readline sqlite wget
+brew install brew-cask gnu-sed maven pcre readline sqlite wget tree
 brew install gdbm htop-osx nginx pgtune python rename stow xz postgresql Caskroom/cask/pgadmin3 Caskroom/cask/keepassx
