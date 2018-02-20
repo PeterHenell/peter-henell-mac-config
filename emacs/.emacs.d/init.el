ba816afc09d39dba3b5d44f4d43c437e41be0577
Peter Henell <a03pethe@gmail.com>
2018-02-20 10:28:17 +0100

    Update init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -37,54 +37,6 @@
 (setq backup-directory-alist `(("." . "~/.emacs_saves")))
 (setq backup-by-copying t)
 (setq delete-old-versions t
-  kept-new-versions 6
-  kept-old-versions 2
-  version-control t)
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-
-(package-initialize)
-
-(require 'ido)
-(ido-mode t)
-(setq ido-enable-flex-matching t)
-
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-(setq uniquify-separator "/")
-(setq uniquify-after-kill-buffer-p t)
-(setq uniquify-ignore-buffers-re "^\\*")
-
-(setq frame-title-format '(buffer-file-name "%f" ("%b")))
-
-(defun disable-magit-highlight-in-buffer ()
-  (face-remap-add-relative 'magit-item-highlight '()))
-(add-hook 'magit-status-mode-hook 'disable-magit-highlight-in-buffer)
-
-(global-set-key (kbd "C-x g") 'magit-status)
-(add-hook 'magit-log-edit-mode-hook
-          '(lambda ()
-             (shell-command "./.git/hooks/prepare-commit-msg")))
-
-(load-theme 'ir-black t)
-
-(put 'downcase-region 'disabled nil)
-
-(defun set-erlang-indent (level)
-  (interactive "nSet erlang indent level to: ")
-  (setq erlang-indent-level level))
-
-(defun switch-erlang-indent ()
-  (interactive)
-  (if (equal erlang-indent-level 2)
-      (setq erlang-indent-level 4)
-    (setq erlang-indent-level 2)))
-
-
-(defun my-cut-function (text &optional rest)
-  (let ((process-connection-type nil))
-    (start-process "emacs_share_buffer.sh" "*Messages*" "emacs_share_buffer.sh" text)))
-
-(setq interprogram-cut-function 'my-cut-function)
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
