a98719ba38952f2643d6df0856c7d45999402c07
Peter Henell <a03pethe@gmail.com>
2018-03-26 14:47:41 +0200

    added melpa

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -38,3 +38,11 @@
       kept-new-versions 6
       kept-old-versions 2
       version-control t)
+
+;; load emacs 24's package system. Add MELPA repository.
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+   '("melpa" . "http://melpa.milkbox.net/packages/")
